09bbd55e87bc7ade30c9fb22fa49b0d962823a9f
dzoladz <derek@derekzoladz.com>
2019-08-23 19:21:14 -0400

    initial framework for script

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,1 +1,114 @@
+#!/usr/local/bin/python3
+# Authors: Derek Zoladz & Ryan Scott
+#
+# Description: Data clean-up for the Academic Library Association of Ohio (ALAO)
+# Notes: Output files will be in .csv format
+#
+###############################################
+# SET SCRIPT VARIABLES
+input_filename = 'ALAOdata.csv'
+output_filename = 'ALAOdata-clean.csv'
+
+###############################################
+
+# IMPORT STATEMENTS
 import pandas as pd
+import os
+
+wa_schema = [
+    'Member ID',
+    'First name',
+    'Last name',
+    'Organization',
+    'Preferred Email',
+    'Preferred Phone',
+    'Preferred Mail Option',
+    'Password,Job title',
+    'Library or Department Within Parent Organization',
+    'Work Address 1',
+    'Work Address 2',
+    'Work City',
+    'Work Province/State',
+    'Work Postal Code',
+    'Work Country',
+    'Work Email Address',
+    'Work Phone',
+    'Work Cellular Phone',
+    'Work Fax Number',
+    'Home Address 1',
+    'Home Address 2',
+    'Home City',
+    'Home Province/State',
+    'Home Postal Code',
+    'Home Country',
+    'Home Email Address',
+    'Home Phone',
+    'Home Cellular Phone',
+    'Home Fax Number',
+    'ACRL Member',
+    'Interest Groups',
+    'Membership History (2010 & prior)',
+    'Retired,Group participation',
+    'Current Leadership Positions',
+    'Past Leadership Positions',
+    'Current Committees',
+    'Past Committees',
+    'ALAO Awards',
+    'Directory listing text',
+    'Expected Graduation Date',
+    'School Attending',
+    'Archived',
+    'Subscribed to emails',
+    'Subscription source',
+    'Opted in',
+    'Event announcements',
+    'Member emails and newsletters',
+    'Administration access',
+    'Created on',
+    'Profile last updated',
+    'Last login',
+    'Updated by',
+    'Balance',
+    'Total donated',
+    'Membership enabled',
+    'Membership level',
+    'Membership status',
+    'Member since',
+    'Renewal due',
+    'Renewal date last changed',
+    'Level last changed',
+    'Access to profile by others',
+    'Details to show',
+    'Photo albums enabled',
+    'Member bundle ID or email',
+    'Member role'
+]
+
+# FUNCTION DEFINITIONS
+
+def grab_df():
+    cd = os.getcwd()
+    input_file = cd + '/' + input_filename
+    df = pd.read_csv(input_file)
+    return df
+
+def write_output_file(df):
+    cd = os.getcwd()
+    output_file = cd + '/' + output_filename
+    df.to_csv(output_file, encoding='utf-8')
+
+
+# MAIN PROGRAM
+
+def main():
+    # get the data
+    data = grab_df()
+
+    # do stuff with the data
+
+
+    # write the data back to file
+    write_output_file(data)
+
+if __name__ == "__main__":
+    main()
